42ff56d575d711e8233e62434b466f5ea88e6ad7
joffreynk <joyjoffrey@gmail.com>
2022-11-01 12:41:56 +0200

    Add validations to vehicles

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,5 +3,7 @@
 class Vehicle < ApplicationRecord
   belongs_to :user
 
-  
+  validates :picture, presence: true, length: { minimum: 10 }
+  validates :price_per_day, presence: true, numericality: true
+  validates :name, :model, presence: true, length: { minimum: 3 }
 end
