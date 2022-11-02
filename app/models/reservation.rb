fbdce78ba6391b265858ced38820c966af8a812c
joffreynk <joyjoffrey@gmail.com>
2022-11-02 19:44:48 +0200

    add validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,7 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :vehicle
 
-  validates 
+  validates :reservation_date, :return_date, presence: true
+  validates :city, presence: true, length: {minimum:3}
+  validates :total_fee, presence:true, numericality:true
 end
