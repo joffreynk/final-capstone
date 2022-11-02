c59ea390e503df9c3fe8f8f028ecdd14cfc25e56
joffreynk <joyjoffrey@gmail.com>
2022-11-02 19:54:38 +0200

    create reservation strong paramaters

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,13 +1,22 @@
 class Api::V1::ReservationsController < ApplicationController
+
   def index
+    render json: Reservation.all, status: 201
   end
 
   def create
+
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+
+  def reserv_params
+    params.permit(:return_date, :reservation_date, :city, :total_fee)
   end
 end
