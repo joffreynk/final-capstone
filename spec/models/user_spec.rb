ef57e038ef27187caa4147cb69ca96be383f53c5
joffreynk <joyjoffrey@gmail.com>
2022-11-05 22:12:36 +0200

    add test to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.new(
+      name: 'Joffrey',
+      email: 'joffrey@email.com',
+      password: '112233',
+      user_name: 'Joffrey1',
+    )
+    @user.save!
+  end
+
+  context 'User Model Properties' do
+    it 'name should be present' do
+      @user.name = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'user name should be present' do
+      @user.email = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'password should be present' do
+      @user.password = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'user name should be unique' do
+      @duplicate_user = User.new(
+        name: 'Joffrey',
+        email: 'joffrey@email.com',
+        password: '112233',
+        user_name: 'Joffrey1'
+      )
+      expect(@duplicate_user).to_not be_valid
+    end
+  end
 end
