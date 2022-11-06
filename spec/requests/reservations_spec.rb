631062a49b09c3e4292085ac5ab1642f19e78742
joffreynk <joyjoffrey@gmail.com>
2022-11-06 19:11:16 +0200

    add test to reservations controller

--- spec/requests/reservations_spec.rb
+++ spec/requests/reservations_spec.rb
@@ -1,7 +1,28 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "Reservations", type: :request do
-#   describe "GET /index" do
-#     pending "add some examples (or delete) #{__FILE__}"
-#   end
-# end
+RSpec.describe "Reservations", type: :request do
+  before(:each) do
+    @user = User.create(
+      name: 'Joffrey',
+      email: 'joffrey@email.com',
+      password: '112233',
+      user_name: 'Joffrey4',
+			role: 'admin',
+    )
+    post '/api/v1/auth/login', params: {user_name: 'Joffrey4', password: '112233'}
+    @token =  JSON.parse(response.body)["token"]
+    @headers = { "ACCEPT": "application/json", "Authorization": @token}
+  end
+
+	context '/api/v1/reservations (get #index)' do
+		it 'should have status 200' do
+			get '/api/v1/reservations', headers: @headers
+			expect(response).to have_http_status(201)
+		end
+
+		it 'show method should be success' do
+			get '/api/v1/reservations', headers: @headers
+			expect(JSON.parse(response.body).length).to eq(0)
+		end
+	end
+end
