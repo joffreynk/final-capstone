6a9600a2a79bb323bf738b86b432dc5889d6e800
joffreynk <joyjoffrey@gmail.com>
2022-11-06 19:10:40 +0200

    add test to hehicles

--- spec/requests/vehicles_spec.rb
+++ spec/requests/vehicles_spec.rb
@@ -1,7 +1,40 @@
-# require 'rails_helper'
+require 'rails_helper'
+include Rails.application.routes.url_helpers
 
-# RSpec.describe "Vehicles", type: :request do
-#   describe "GET /index" do
-#     pending "add some examples (or delete) #{__FILE__}"
-#   end
-# end
+RSpec.describe "Vehicles", type: :request do
+	before(:each) do
+    @user = User.create(
+      name: 'Joffrey',
+      email: 'joffrey@email.com',
+      password: '112233',
+      user_name: 'Joffrey4',
+			role: 'admin',
+    )
+		# Vehicle.create(name: 'BMW', price_per_day: 23.3, model: 'BMW', user_id:  @user.id)
+		# Vehicle.create(name: 'BMW1', price_per_day: 23.3, model: 'BMW',  user_id:  1)
+		# Vehicle.create(name: 'BMW2', price_per_day: 23.3, model: 'BMW',  user_id:  1)
+
+    post '/api/v1/auth/login', params: {user_name: 'Joffrey4', password: '112233'}
+    @token =  JSON.parse(response.body)["token"]
+    @headers = { "ACCEPT": "application/json", "Authorization": @token}
+		
+  end
+
+	context '/api/v1/vehicles (get #index)' do
+
+		it 'should have status 200' do
+			get '/api/v1/vehicles', headers: @headers
+			expect(response).to have_http_status(200)
+		end
+
+		# it 'should have status 200' do
+		# 	get '/api/v1/vehicles', headers: @headers
+		# 	expect(JSON.parse(response.body).length).to eq(3)
+		# end
+
+		it 'show method should be success' do
+			get '/api/v1/vehicles/1', headers: @headers
+			expect(response).to have_http_status(200)
+		end
+	end
+end
