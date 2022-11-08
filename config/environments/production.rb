60b36939f15e48cfdf34a7b51bd9c940617d5a4d
joffreynk <joyjoffrey@gmail.com>
2022-11-08 23:01:37 +0200

    remove default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,5 @@
 require "active_support/core_ext/integer/time"
 
-Rails.application.routes.default_url_options = {
-  host: 'https://vehicles-booking-capstone-app.herokuapp.com/api/v1/'
-}
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
