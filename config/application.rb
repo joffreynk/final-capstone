192f557189de7a230203fa415225e6337c535e18
joffreynk <joyjoffrey@gmail.com>
2022-11-09 19:09:19 +0200

    remve asset in application

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,6 @@ module FinalCapstone
     # config.time_zone = "Central Time (US & Canada)"
     config.eager_load_paths << Rails.root.join("extras")
 
-    config.assets.initialize_on_precompile = false
-
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
